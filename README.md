74f84d536dce7a8ff06b97864db91f4872798077
James Chen <unknownguy2002@gmail.com>
2019-02-25 05:14:44 +0800

    Updated readme.md + added input.json(Not working yet)

--- README.md
+++ README.md
@@ -10,8 +10,15 @@ website and performs facial recognition
 - lxml
 - request
 - cssselect
+- bs4
 
 ## Notes:
 - This is extremely unethical and I'd advise you not to use this seriously.
 - I am not responsible for anything you may find about someone.
 - I hope you use this tool responsibly
+
+## Features:
+- [x] Scrap and get photos
+- [x] Index Photos
+- [ ] Accept input from file
+- [ ] Perform facial recognition
--- main.py
+++ main.py
@@ -3,6 +3,10 @@ from lxml.html import fromstring
 import requests
 from bs4 import BeautifulSoup
 import re
+import imghdr
+import os
+import json
+
 linkgex = re.compile(
         r'^(?:http|ftp)s?://' # http:// or https://
         r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|' #domain...
@@ -12,7 +16,7 @@ linkgex = re.compile(
         r'(?:/?|[/?]\S+)$', re.IGNORECASE)
 
 #Start of program
-name = "Saint Joseph's Institution"
+name = "James Chen"
 
 def getpage(query, page=0):
     encoded = urlencode({'q':query, 'start':page*10})
@@ -30,9 +34,14 @@ def getimagesurl(site):
 
     return urls
 
+harvest = {}
+with open('./output/harvested.json', 'w') as outfile:
+    json.dump(harvest, outfile)
 
-for i in range(1):
+for i in range(3):
+    r = 0
     for result in getpage(name, i).cssselect(".r a"):
+        r = r + 1
         url = result.get("href")
         if url.startswith("/url?"):
             url = parse_qs(urlparse(url).query)['q']
@@ -46,19 +55,34 @@ for i in range(1):
                     try:
                         if imgurl['src']:
                             parseurl = imgurl['src']
-                            filename = re.search(r'([\w_-]+[.](jpg|gif|png))$', imgurl['src'])
-                            if imgurl['src'][0] == "/":
+                            if 'http' not in imgurl['src']:
                                 # sometimes an image source can be relative
-                                if imgurl['src'][0] == "/"or'\\':
+                                if (imgurl['src'][0] == "//" or "\\\\") and (imgurl['src'][1] == "//" or "\\\\"):
+                                    print("1")
+                                    parseurl = "http://"+(imgurl['src'][2:])
+                                elif imgurl['src'][0] == "/" or "\\":
+                                    print("2")
                                     netloc = urlparse(url[0]).netloc
                                     parseurl = "http://"+netloc+str(imgurl['src'])
                                 else:
+                                    print("3")
                                     parseurl = url[0]+str(imgurl['src'])
+                            print(url[0])
+                            print(imgurl['src'])
                             print(parseurl)
                             response = requests.get(parseurl)
-                            print(filename.group(1))
-                            with open("./output/"+filename.group(1), 'wb') as f:
+                            filename = str(i)+"-"+str(r)+"-"+str(y)
+                            harvest[str(i)+"-"+str(r)+"-"+str(y)] = {"url": url[0], "img": imgurl['src'], "parsed": parseurl}
+                            jsonf = open("./output/harvested.json", "w")
+                            jsonf.write(json.dumps(harvest, indent=4, sort_keys=True))
+                            jsonf.close()
+                            with open("./output/"+filename+".check", 'wb') as f:
                                 f.write(response.content)
+                            itype = imghdr.what("./output/"+filename+".check")
+                            if not itype:
+                                # Cases where cannot read image type. Most of the times it's SVG
+                                itype = "svg"
+                            os.rename("./output/"+filename+".check", "./output/"+filename+"."+itype)
                     except Exception as e:
                         print(e)
                         pass
